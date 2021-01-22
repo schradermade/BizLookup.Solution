df59899b95fb1a5fc64b904e69897fe04de90fb1
Nathan Schrader <nathanschrader@icloud.com>
2021-01-22 15:23:10 -0800

    add GetDetails method to Business class

--- BizLookupMvc/Models/Business.cs
+++ BizLookupMvc/Models/Business.cs
@@ -4,29 +4,41 @@ namespace BizLookupMvc.Models
 {
     public class Business
     {
-        public int BusinessId { get; set; }
-        [Required]
-        [StringLength(50)]
-        public string Name { get; set; }
-        [Required]
-        [StringLength (50)]
-        public string Industry { get; set; }
-        [Required]
-        [StringLength (50)]
+      public int BusinessId { get; set; }
+      [Required]
+      [StringLength(50)]
+      public string Name { get; set; }
+      [Required]
+      [StringLength (50)]
+      public string Industry { get; set; }
+      [Required]
+      [StringLength (50)]
         // [Range(0, 200, ErrorMessage = "Industry must be between 0 and 200.")]
-        public string Address { get; set; }
-        [Required]
-        public string Hours { get; set; }
+      public string Address { get; set; }
+      [Required]
+      public string Hours { get; set; }
 
-        public static List<Business> GetBusinesses()
-        {
-          var apiCallTask = ApiHelper.GetAll();
-          var result = apiCallTask.Result;
+      public static List<Business> GetBusinesses()
+      {
+        var apiCallTask = ApiHelper.GetAll();
+        var result = apiCallTask.Result;
 
-          JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
-          List<Business> businessList = JsonConvert.DeserializeObject<List<Business>>(jsonResponse.ToString());
+        JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+        List<Business> businessList = JsonConvert.DeserializeObject<List<Business>>(jsonResponse.ToString());
 
-          return businessList;
-        }
+        return businessList;
+      }
+
+      public static Animal GetDetails(int id)
+      {
+        var apiCallTask = ApiHelper.Get(id);
+        var result = apiCallTask.Result;
+
+        JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+        Animal animal = JsonConvert.DeserializeObject<Animal>(jsonResponse.ToString());
+
+        return animal;
+      }
+
     }
 }
