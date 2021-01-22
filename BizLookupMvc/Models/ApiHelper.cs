c12053ab5889ce425cb8ea266fe500d86948f20b
Nathan Schrader <nathanschrader@icloud.com>
2021-01-22 15:47:57 -0800

    add [public async Task Delete] helper method to ApiHelper class

--- BizLookupMvc/Models/ApiHelper.cs
+++ BizLookupMvc/Models/ApiHelper.cs
@@ -23,12 +23,22 @@ namespace BizLookupMvc.Models
     }
 
     public static async Task Put(int id, string newBusiness)
-      {
-        RestClient client = new RestClient("http://localhost:5000/api");
-        RestRequest request = new RestRequest($"businesses/{id}", Method.PUT);
-        request.AddHeader("Content-Type", "application/json");
-        request.AddJsonBody(newBusiness);
-        var response = await client.ExecuteTaskAsync(request);
-      }
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"businesses/{id}", Method.PUT);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newBusiness);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task Delete(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"businesses/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    
   }
 }
