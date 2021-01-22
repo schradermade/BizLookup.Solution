ba492bad6901ea6b208ddafca37a8e55483fdaf3
Nathan Schrader <nathanschrader@icloud.com>
2021-01-22 15:39:11 -0800

    add public async 'Put' method passing in arguments of 'id' and 'newBusiness'

--- BizLookupMvc/Models/ApiHelper.cs
+++ BizLookupMvc/Models/ApiHelper.cs
@@ -21,5 +21,14 @@ namespace BizLookupMvc.Models
       request.AddJsonBody(newBusiness);
       var response = await client.ExecuteTaskAsync(request);
     }
+
+    public static async Task Put(int id, string newBusiness)
+      {
+        RestClient client = new RestClient("http://localhost:5000/api");
+        RestRequest request = new RestRequest($"businesses/{id}", Method.PUT);
+        request.AddHeader("Content-Type", "application/json");
+        request.AddJsonBody(newBusiness);
+        var response = await client.ExecuteTaskAsync(request);
+      }
   }
 }
