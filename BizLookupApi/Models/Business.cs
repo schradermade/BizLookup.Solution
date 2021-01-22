d407c77c51c99ff08120b43ca7de2f1241505121
Nathan Schrader <nathanschrader@icloud.com>
2021-01-22 11:33:47 -0800

    comment out Range attribute

--- BizLookupApi/Models/Business.cs
+++ BizLookupApi/Models/Business.cs
@@ -6,13 +6,13 @@ namespace BizLookupApi.Models
     {
         public int BusinessId { get; set; }
         [Required]
-        [StringLength(20)]
+        [StringLength(30)]
         public string Name { get; set; }
         [Required]
         [StringLength (40)]
         public string Industry { get; set; }
         [Required]
-        [Range(0, 200, ErrorMessage = "Age must be between 0 and 200.")]
+        // [Range(0, 200, ErrorMessage = "Industry must be between 0 and 200.")]
         public string Address { get; set; }
         [Required]
         public string Hours { get; set; }
