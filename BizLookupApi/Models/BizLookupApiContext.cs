083c45f6482a8bda7f41d7ce49e44cea4e66709c
Nathan Schrader <nathanschrader@icloud.com>
2021-01-22 17:25:27 -0800

    record no change

--- BizLookupApi/Models/BizLookupApiContext.cs
+++ BizLookupApi/Models/BizLookupApiContext.cs
@@ -10,9 +10,8 @@ namespace BizLookupApi.Models
         }
 
         public DbSet<Business> Businesses { get; set; }
-
         protected override void OnModelCreating(ModelBuilder builder)
-{
+        {
         builder.Entity<Business>()
             .HasData(
                 new Business { BusinessId = 1, Name = "Blue Dolphin Wash", Industry = "Retail", Address = "Newberg, Oregon", Hours = "8am-6pm, 7 days/Week" },
@@ -21,6 +20,6 @@ namespace BizLookupApi.Models
                 new Business { BusinessId = 4, Name = "RV's To-Go", Industry = "Hospitality", Address = "Wilsonville, Oregon", Hours = "8am-5pm, 7 days/Week" },
                 new Business { BusinessId = 5, Name = "Chanel's Hair Choppin", Industry = "Retail", Address = "Wilsonville, Oregon", Hours = "8am-6pm, Mon-Thurs" }
             );
-}
+        }
     }
 }
