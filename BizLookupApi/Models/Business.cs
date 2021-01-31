1e99d26a7d80fdbd5e1c2bd81bc5a0e56009e45e
Nathan Schrader <nathanschrader@icloud.com>
2021-01-31 15:18:15 -0800

    fix functionality to add biz listing

--- BizLookupApi/Models/Business.cs
+++ BizLookupApi/Models/Business.cs
@@ -12,11 +12,11 @@ namespace BizLookupApi.Models
         [Required]
         [StringLength (50)]
         public string Industry { get; set; }
-        [Required]
+    
         [StringLength (50)]
         // [Range(0, 200, ErrorMessage = "Industry must be between 0 and 200.")]
         public string Address { get; set; }
-        [Required]
+        
         public string Hours { get; set; }
         public string URL { get; set; }
         public string Phone { get; set; }
