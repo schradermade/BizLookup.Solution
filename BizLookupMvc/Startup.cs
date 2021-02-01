c90043ed10ef1b9900d54529da5e232d10c0da8a
Nathan Schrader <nathanschrader@icloud.com>
2021-01-31 16:55:44 -0800

    untracked changes to commit

--- BizLookupMvc/Startup.cs
+++ BizLookupMvc/Startup.cs
@@ -31,7 +31,6 @@ namespace BizLookupMvc
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
